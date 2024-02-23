9fd2eb411dac22049faab29abd2b2cfd3c32b040
ManelHajHassin <manelhadjhassin@gmail.com>
2024-02-23 12:21:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import img1 from './Image_1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div class="div1">
+        <img classe="img1"src={img1} ></img>
+    <h2 class="p1">Blog post published</h2>
+    <p class="p2">this blog has been published . team memberss will be able to edit post and republish changes </p>
+    <a class="p3"href="#">link</a>
+    <div class="div2">
+    <button class="btn btn1">Cancel</button>
+   <button class="btn btn2">Confirm</button>
+    </div>
+
+  
+  
+    </div>
+  
+
+  );
+}
+
+export default App;
